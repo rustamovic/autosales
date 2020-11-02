de392fd48d6f420f20c9e633963a1eff622879a6
Khadjiakbar Rustamovic <rustamovic@yahoo.com>
2020-11-02 11:41:16 +0500

    qwertyuio

---
+++ src/Components/LowerPart/LowerPart.js
@@ -0,0 +1,53 @@
+import React from "react";
+import tw from "twin.macro";
+import {
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBIcon,
+  MDBBtn,
+  MDBInput,
+  MDBContainer,
+} from "mdbreact";
+import { SectionHeading, Subheading as SubheadingBase } from "../Misk/Headings";
+import "mdbreact/dist/css/mdb.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+const Heading = tw(SectionHeading)``;
+const ContactPage = () => {
+  return (
+    <MDBContainer className="my-5">
+      <Heading> Questions?</Heading>
+      <MDBRow className="mt-5 mb-3">
+        <MDBCol>
+          <MDBRow className="text-center">
+            <MDBCol md="4">
+              <MDBBtn tag="a" floating color="blue" className="accent-1">
+                <MDBIcon icon="map-marker-alt" />
+              </MDBBtn>
+              <p>New York, 94126</p>
+              <p className="mb-md-0">United States</p>
+            </MDBCol>
+            <MDBCol md="4">
+              <MDBBtn tag="a" floating color="blue" className="accent-1">
+                <MDBIcon icon="phone" />
+              </MDBBtn>
+              <p>+ 01 234 567 89</p>
+              <p className="mb-md-0">Mon - Fri, 8:00-22:00</p>
+            </MDBCol>
+            <MDBCol md="4">
+              <MDBBtn tag="a" floating color="blue" className="accent-1">
+                <MDBIcon icon="envelope" />
+              </MDBBtn>
+              <p>info@gmail.com</p>
+              <p className="mb-md-0">sale@gmail.com</p>
+            </MDBCol>
+          </MDBRow>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
+};
+
+export default ContactPage;
