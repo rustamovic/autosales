d9b1a4613b927cf56db924d3a162dee67513c1b9
Khadjiakbar Rustamovic <rustamovic@yahoo.com>
2020-11-05 14:10:41 +0500

    Loader changed

--- src/routes/Loader/Loading.js
+++ src/routes/Loader/Loading.js
@@ -5,31 +5,16 @@ class Loading extends Component {
     return (
       <div id="lo">
         <div id="lol">
-          <div id="container">
-            <div class="divider" aria-hidden="true"></div>
-            <p class="loading-text" aria-label="Loading">
-              <span class="letter" aria-hidden="true">
-                L
-              </span>
-              <span class="letter" aria-hidden="true">
-                o
-              </span>
-              <span class="letter" aria-hidden="true">
-                a
-              </span>
-              <span class="letter" aria-hidden="true">
-                d
-              </span>
-              <span class="letter" aria-hidden="true">
-                i
-              </span>
-              <span class="letter" aria-hidden="true">
-                n
-              </span>
-              <span class="letter" aria-hidden="true">
-                g
-              </span>
-            </p>
+          <div className="banter-loader">
+            <div className="banter-loader__box"></div>
+            <div className="banter-loader__box"></div>
+            <div className="banter-loader__box"></div>
+            <div className="banter-loader__box"></div>
+            <div className="banter-loader__box"></div>
+            <div className="banter-loader__box"></div>
+            <div className="banter-loader__box"></div>
+            <div className="banter-loader__box"></div>
+            <div className="banter-loader__box"></div>
           </div>
         </div>
       </div>
