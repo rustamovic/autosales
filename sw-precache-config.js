f3be0b4ab5921e0b4a35a33e1d95817c154ba236
Abbos Iskandarov <iskandarov.foto@gmail.com>
2020-11-14 19:48:21 +0500

    fixed again

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -4,7 +4,7 @@ module.exports = {
     'build/static/js/**.js'
   ],
   swFilePath: './build/service-worker.js',
-  templateFilePath: './service-worker.js',
+  // templateFilePath: './service-worker.tmpl',
   stripPrefix: 'build/',
   handleFetch: false,
   runtimeCaching: [{
