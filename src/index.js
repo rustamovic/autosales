e69bcb7322f52fcc2edaffbbd69f7ff2236a1afe
Abbos Iskandarov <iskandarov.foto@gmail.com>
2020-11-16 01:23:25 +0500

    added and fixed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM, {render, hydrate} from "react-dom";
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import "./index.css";
 import "leaflet/dist/leaflet.css";
@@ -7,21 +7,16 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { store } from "./Redux/Store";
 
-const rootElement = document.getElementById("root")
 
-if (rootElement.hasChildNodes()) {
- hydrate(<React.StrictMode>
-  <Provider store={store}>
-    <App />
-  </Provider>
-</React.StrictMode>, rootElement) 
-}else{
-  render(<React.StrictMode>
+
+ReactDOM.render(
+  <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>, rootElement)
-}
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
