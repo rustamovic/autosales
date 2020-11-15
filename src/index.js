2a60d63ec0f2639d69623101f2b0e0406ac7b53f
Abbos Iskandarov <iskandarov.foto@gmail.com>
2020-11-16 00:44:43 +0500

    added react-snap and react-helmet

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM, {render, hydrate} from "react-dom";
 import { Provider } from "react-redux";
 import "./index.css";
 import "leaflet/dist/leaflet.css";
@@ -7,14 +7,22 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { store } from "./Redux/Store";
 
-ReactDOM.render(
-  <React.StrictMode>
+const rootElement = document.getElementById("root")
+
+if (rootElement.hasChildNodes()) {
+ hydrate(<React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
+</React.StrictMode>, rootElement) 
+}else{
+  render(<React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+  </React.StrictMode>, rootElement)
+}
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
