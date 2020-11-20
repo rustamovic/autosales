1bb02bb6777b6360fb7da2b24d6d1dfa8c95c553
Abbos Iskandarov <iskandarov.foto@gmail.com>
2020-11-20 17:23:05 +0500

    react app is optimized

---
+++ config-overrides.js
@@ -0,0 +1,36 @@
+const path = require('react');
+const {override} = require('customize-cra')
+
+
+const options = {
+  stylesDir: path.join(__dirname, './src/index.css'),
+  themeVariables: [
+    '@primary-color',
+    '@secondary-color',
+    '@text-color',
+    '@heading-color',
+    '@nav-dark-bg',
+    '@header-text-color',
+    '@layout-header-background',
+    '@layout-footer-background',
+    '@nav-dark-text-color',
+    '@hor-nav-text-color',
+    '@nav-header-selected-text-color'
+  ],
+  indexFileName: 'index.html',
+  generateOnce: false // generate color.less on each compilation
+};
+
+
+const overrideProcessEnv = value => config => {
+  config.resolve.modules = [
+    path.join(__dirname, 'src')
+  ].concat(config.resolve.modules);
+  return config;
+};
+
+module.exports = override(
+  overrideProcessEnv({
+    VERSION: JSON.stringify(require('./package.json').version),
+  })
+);
